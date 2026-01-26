8cc852b1b6d4fcf99a7b10ddc28136955ede9ebd
maltelueken <malte_lueken@arcor.de>
2026-01-26 10:22:46 +0100

    Add empirical fMRI tutorial

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ myst_enable_extensions = [
 
 nb_merge_streams = True
 nb_execution_mode = "cache"
-nb_execution_timeout = 120
+nb_execution_timeout = 500
 nb_execution_raise_on_error = True
 nb_scroll_outputs = True
 
