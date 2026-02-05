40e5696680a401417a423fcacea66f5c5cf088da
maltelueken <malte_lueken@arcor.de>
2026-02-05 10:36:20 +0100

    Skip regression tests for fitters for torch backend and windows

--- tests/fitters/conftest.py
+++ tests/fitters/conftest.py
@@ -1,10 +1,24 @@
 """Setup for fitter tests."""
 
+import sys
+import keras
 import pandas as pd
 import pytest
 from prfmodel.models.gaussian import Gaussian2DPRFModel
 from prfmodel.models.impulse import DerivativeTwoGammaImpulse
 from tests.conftest import StimulusSetup
+
+# For the torch backend, the regression test fails due to slight numeric differences that cannot easily be captured
+# with tolerances
+skip_torch = pytest.mark.skipif(
+    keras.backend.backend == "torch",
+    reason="Slight numerical differences in parameter estimates with torch backend",
+)
+
+skip_windows = pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="Slight numerical differences in parameter estimates on Windows",
+)
 
 parametrize_dtype = pytest.mark.parametrize("dtype", [None, "float32"])
 
