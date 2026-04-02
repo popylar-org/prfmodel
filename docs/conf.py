9fce7a8fdfe74aa73aa24987e2a27811482b8749
maltelueken <malte_lueken@arcor.de>
2026-04-02 15:13:12 +0200

    Fix HTML rendering warnings

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ autoapi_template_dir = "_templates"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "_templates"]
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
